1459024e21339e67795095ed6d3db5ebb097e594
이근호 <khlee84@hamonsoft.co.kr>
2025-03-01 16:13:17 +0900

    initial commit ...

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  typescript: {
+    shim: false,
+    typeCheck: false,
+  },
+  devtools: { enabled: true },
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
+  },
+});
